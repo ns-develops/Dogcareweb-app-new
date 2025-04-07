863ae3d6b1474e126a53dee87d25657a45028ed1
”ns-develops” <test.tt9061030@gmail.com>
2025-04-08 01:13:41 +0200

    Alla hundarna ready

--- src/pages/Allahundar.jsx
+++ src/pages/Allahundar.jsx
@@ -19,39 +19,41 @@ function Allahundar() {
   }, []); 
 
   return (
-    <div className="dog-list-container">
-      <h2>Alla Hundarna</h2>
-      <div className="dogs-gallery">
-        {dogs.length === 0 ? (
-          <h1>Laddar...</h1>
-        ) : (
-          <div className="container">
-            <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
-              {dogs.map((dog) => (
-                <Link key={dog.chipNumber} to={`/dog/${dog.chipNumber}`}>
-                  <article className="border p-5 rounded my-10">
-                    <img
-                      src={dog.img}
-                      alt={dog.name}
-                      loading="lazy"
-                      className="rounded md:h-72 w-full object-cover"
-                    />
-                    <h3>Chip number: {dog.chipNumber}</h3>
-                    <h3>Name: {dog.name}</h3>
-                    <h3>Age: {dog.age}</h3>
-                    <h3>Sex: {dog.sex}</h3>
-                    <h3>Breed: {dog.breed}</h3>
-                    {dog.present ? (
-                    <h3 className="text-green-600">Current: This dog is present.</h3>
-                    ) : (
-                    <h3 className="text-red-600">Current: This dog is not present.</h3>
-          )}
-                  </article>
-                </Link>
-              ))}
+    <div className="main-content">
+      <div className="dog-list-container">
+        <h2 className="text-center text-3xl font-bold mb-8"></h2>
+        <div className="dogs-gallery">
+          {dogs.length === 0 ? (
+            <h1>Laddar...</h1>
+          ) : (
+            <div className="container mx-auto px-4">
+              <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+                {dogs.map((dog) => (
+                  <Link key={dog.chipNumber} to={`/dog/${dog.chipNumber}`}>
+                    <article className="border p-5 rounded-lg shadow-lg bg-white hover:shadow-2xl transition-all">
+                      <img
+                        src={dog.img}
+                        alt={dog.name}
+                        loading="lazy"
+                        className="rounded-md md:h-72 w-full object-cover mb-4"
+                      />
+                      <h3>Chip number: {dog.chipNumber}</h3>
+                      <h3>Name: {dog.name}</h3>
+                      <h3>Age: {dog.age}</h3>
+                      <h3>Sex: {dog.sex}</h3>
+                      <h3>Breed: {dog.breed}</h3>
+                      {dog.present ? (
+                        <h3 className="text-green-600">Current: This dog is present.</h3>
+                      ) : (
+                        <h3 className="text-red-600">Current: This dog is not present.</h3>
+                      )}
+                    </article>
+                  </Link>
+                ))}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
